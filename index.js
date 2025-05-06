4d898119e21f6348cb7b9670b604d97d659d2262
Katarina Packova <katarina.packova@gmail.com>
2025-05-06 13:19:12 +0200

    Update

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const displayElm = document.querySelector('.display');
 
+displayElm.textContent = 42;
+
+const handleDigitClick = (event) => {
+  console.log('Uživatel klikl na tlačítko');
+};
+
+document.querySelector('.keypad').addEventListener('click', handleDigitClick);
